121d6c12cc914390b2d13447f663f07c77166b66
Anish Araz <anisharaz919@gmail.com>
2025-02-14 16:49:33 +0530

    orchestrator | FEAT : add different actions handlers

---
+++ orchestrator/cli/subscriber/actions/dnsActions.go
@@ -0,0 +1,100 @@
+package actions
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func DnsActionsSubscriber(conn *amqp.Connection) {
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+	defer conn.Close()
+
+	q, err := ch.QueueDeclare(
+		"dns_actions", // name
+		true,          // durable
+		false,         // delete when unused
+		false,         // exclusive
+		false,         // no-wait
+		nil,           // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(
+		3,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "Failed to set QoS")
+
+	works, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	failOnError(err, "Failed to register a consumer")
+	acks := make(chan *amqp.Delivery)
+
+	notifyConnClosed := make(chan *amqp.Error)
+	conn.NotifyClose(notifyConnClosed)
+
+	notifyChannelClosed := make(chan *amqp.Error)
+	ch.NotifyClose(notifyChannelClosed)
+
+	go func() {
+		for {
+			select {
+			case work := <-works:
+				go dnsActionsWorker(acks, &work)
+			case ack := <-acks:
+				// The false in the Ack() function states that it should only acknowledge this single message
+				// If true it will ack all message if they are not processed
+				ackError := ack.Ack(false)
+				if ackError != nil {
+					log.Fatalf("Ack error: %v", ackError)
+				}
+			}
+		}
+	}()
+
+	log.Printf("LISTENING-- DNS actions ")
+
+	select {
+	case connError := <-notifyConnClosed:
+		log.Fatalf("saw connection closure error: %v", connError)
+	case channelError := <-notifyChannelClosed:
+		log.Fatalf("saw channel closure error: %v", channelError)
+	}
+}
+
+func dnsActionsWorker(acks chan<- *amqp.Delivery, work *amqp.Delivery) {
+	var workData map[string]interface{}
+
+	err := json.Unmarshal(work.Body, &workData)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	switch workData["action"] {
+	case "create":
+		fmt.Println(string(work.Body))
+	default:
+		fmt.Println("Unknown action")
+	}
+	acks <- work
+}
