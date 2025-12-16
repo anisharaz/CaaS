8d1cce11c1f45400a8b78e612dc4a0c15619fa41
Anish Araz <anisharaz919@gmail.com>
2025-12-16 19:38:23 +0530

    migration

--- app/components/ui/chart.tsx
+++ app/components/ui/chart.tsx
@@ -94,7 +94,7 @@ ${colorConfig
 }
 `
           )
-          .join("\n"),
+          .join("\n")
       }}
     />
   )
@@ -127,7 +127,7 @@ const ChartTooltipContent = React.forwardRef<
       formatter,
       color,
       nameKey,
-      labelKey,
+      labelKey
     },
     ref
   ) => {
@@ -166,7 +166,7 @@ const ChartTooltipContent = React.forwardRef<
       hideLabel,
       labelClassName,
       config,
-      labelKey,
+      labelKey
     ])
 
     if (!active || !payload?.length) {
@@ -179,7 +179,7 @@ const ChartTooltipContent = React.forwardRef<
       <div
         ref={ref}
         className={cn(
-          "grid min-w-[8rem] items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl",
+          "grid min-w-32 items-start gap-1.5 rounded-lg border border-border/50 bg-background px-2.5 py-1.5 text-xs shadow-xl",
           className
         )}
       >
@@ -216,13 +216,13 @@ const ChartTooltipContent = React.forwardRef<
                                 "w-1": indicator === "line",
                                 "w-0 border-[1.5px] border-dashed bg-transparent":
                                   indicator === "dashed",
-                                "my-0.5": nestLabel && indicator === "dashed",
+                                "my-0.5": nestLabel && indicator === "dashed"
                               }
                             )}
                             style={
                               {
                                 "--color-bg": indicatorColor,
-                                "--color-border": indicatorColor,
+                                "--color-border": indicatorColor
                               } as React.CSSProperties
                             }
                           />
@@ -306,7 +306,7 @@ const ChartLegendContent = React.forwardRef<
                   <div
                     className="h-2 w-2 shrink-0 rounded-[2px]"
                     style={{
-                      backgroundColor: item.color,
+                      backgroundColor: item.color
                     }}
                   />
                 )}
@@ -365,5 +365,5 @@ export {
   ChartTooltipContent,
   ChartLegend,
   ChartLegendContent,
-  ChartStyle,
+  ChartStyle
 }
