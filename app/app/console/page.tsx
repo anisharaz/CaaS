f4fa72b57b25a735b72948081826956725fd89b6
Anish Araz <anisharaz919@gmail.com>
2025-12-16 19:33:19 +0530

    migration

--- app/app/console/page.tsx
+++ app/app/console/page.tsx
@@ -24,7 +24,7 @@ async function ConsolePage() {
 
   return (
     <>
-      <div className="2xl:max-w-screen-2xl xl:max-w-screen-xl lg:max-w-screen-lg md:space-y-6 space-y-2 md:m-auto mx-2">
+      <div className="2xl:max-w-(--breakpoint-2xl) xl:max-w-(--breakpoint-xl) lg:max-w-(--breakpoint-lg) md:space-y-6 space-y-2 md:m-auto mx-2">
         <div className="text-2xl md:text-4xl font-bold md:mt-4 mt-2">
           Console Home
         </div>
--- app/components/ui/sonner.tsx
+++ app/components/ui/sonner.tsx
@@ -1,5 +1,12 @@
 "use client"
 
+import {
+  CircleCheck,
+  Info,
+  LoaderCircle,
+  OctagonX,
+  TriangleAlert,
+} from "lucide-react"
 import { useTheme } from "next-themes"
 import { Toaster as Sonner } from "sonner"
 
@@ -12,6 +19,13 @@ const Toaster = ({ ...props }: ToasterProps) => {
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      icons={{
+        success: <CircleCheck className="h-4 w-4" />,
+        info: <Info className="h-4 w-4" />,
+        warning: <TriangleAlert className="h-4 w-4" />,
+        error: <OctagonX className="h-4 w-4" />,
+        loading: <LoaderCircle className="h-4 w-4 animate-spin" />,
+      }}
       toastOptions={{
         classNames: {
           toast:
@@ -20,8 +34,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
           cancelButton:
-            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground"
-        }
+            "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+        },
       }}
       {...props}
     />
